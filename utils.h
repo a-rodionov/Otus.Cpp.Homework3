3e2df27ab6346cae2366f4a99d27e2c3836a9419
Andrew Rodionov <camelot58@gmail.com>
2018-04-20 13:50:12 -0700

    - для проверки целочисленного типа std::numeric_limits<T>::is_integer заменен на std::is_integral<T>::value.

--- utils.h
+++ utils.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <cstddef>
-#include <limits>
+#include <type_traits>
 
 namespace homework3 {
 
 constexpr auto factorial(auto number) -> decltype(number)
 {
-  static_assert(std::numeric_limits<decltype(number)>::is_integer, "Argument of factorial function must be integer type.");
+  static_assert(std::is_integral<decltype(number)>::value, "Argument of factorial function must be integer type.");
   if(0 > number)
     throw std::invalid_argument("Argument of factorial function must be positive integer.");
 
